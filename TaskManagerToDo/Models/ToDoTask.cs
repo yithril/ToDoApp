74d8111e65fc00b6177ed9e12322c210bae79d10
jhop <jhop@michiganfirst.com>
2021-10-21 11:51:07 -0400

    Required

--- TaskManagerToDo/Models/ToDoTask.cs
+++ TaskManagerToDo/Models/ToDoTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using TaskManagerToDo.Identity;
 
@@ -8,6 +9,8 @@ namespace TaskManagerToDo.Models
     public class ToDoTask
     {
         public int Id { get; set; }
+
+        [Required]
         public string Description { get; set; }
 
         [DisplayName("Due Date")]
